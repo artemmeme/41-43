f58caac6c73ccea4855752828246d842fc913fc1
Артем Шустов <artemmeme2017@yandex.ru>
2022-12-10 23:17:47 +0300

    Написал программу для расчета точки пересечения двух прямых

--- 43/DZ_43.cs
+++ 43/DZ_43.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+double x = 0;
+double y = 0;
+Console.WriteLine("Напишите b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+FindXY(x, b2, b1, k1, k2);
+void FindXY(double x, double b2, double b1, double k1, double k2)
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+    Console.WriteLine($"Координаты точек пересечения - [{x};{y}]");
+}
